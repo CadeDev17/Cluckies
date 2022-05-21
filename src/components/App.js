856ac2f6eebfd230cacbfa4daa9b8f492d75befa
Decryptr <decryptr@Decryptrs-MacBook-Pro.local>
2022-05-21 00:14:31 -0400

    Updated App Website

--- src/components/App.js
+++ src/components/App.js
@@ -535,9 +535,11 @@ function App() {
 
 
 			<section class="teams">
-				<h1 class="title">our <span>team</span> <a href="#">view all >></a></h1>
+				<h1 class="title">our <span>team</span> <a href="#">view all </a></h1>
+			</section>
 
 
+			<section>
 				<div class="box-container">
 
 					<div class="box">
@@ -546,8 +548,8 @@ function App() {
 						</div>
 						<div class="content">
 							<div class="icons">
-								<a class="fas fa-calendar"></i>6+ yrs</a>
-								<a class="fas fa-user"></i>Cluckie Founder & Artist</a>
+								<a class="fas fa-calendar">6+ yrs</a>
+								<a class="fas fa-user">Cluckie Founder / Artist</a>
 							</div>
 							<h3>Master Cluck</h3>
 							<p>Lorem ipsum dolor sit amet, farhan aliqua. Ut enim ad minim veniam, quis</p>
@@ -556,12 +558,12 @@ function App() {
 
 					<div class="box">
 						<div class="img">
-							<img src="img/mainside.png">
+							<img src="img/mainside.png"/>
 						</div>
 						<div class="content">
 							<div class="icons">
-								<a class="fas fa-calendar"></i>10+ yrs</a>
-								<a class="fas fa-user"></i>Cluckster Marketer</a>
+								<a class="fas fa-calendar">10+ yrs</a>
+								<a class="fas fa-user">Cluckster Marketer</a>
 							</div>
 							<h3>Super Cluck</h3>
 							<p>Lorem ipsum dolor sit amet, farhan aliqua. Ut enim ad minim veniam, quis</p>
@@ -570,12 +572,12 @@ function App() {
 
 					<div class="box">
 						<div class="img">
-							<img src="img/owner.png">
+							<img src="img/owner.png"/>
 						</div>
 						<div class="content">
 							<div class="icons">
-								<a class="fas fa-calendar"></i>2+ yrs</a>
-								<a class="fas fa-user"></i>Cluckchain  Developer</a>
+								<a class="fas fa-calendar">2+ yrs</a>
+								<a class="fas fa-user">Cluckchain  Developer</a>
 							</div>
 							<h3>Cluck'n Clucker</h3>
 							<p>I like to cluck... i mean code.</p>
@@ -584,12 +586,12 @@ function App() {
 
 					<div class="box">
 						<div class="img">
-							<img src="img/communitymanager.png">
+							<img src="img/communitymanager.png"/>
 						</div>
 						<div class="content">
 							<div class="icons">
-								<a class="fas fa-calendar"></i>15+yrs</a>
-								<a class="fas fa-user"></i>Community  Manacluck</a>
+								<a class="fas fa-calendar">15+yrs</a>
+								<a class="fas fa-user">Community  Manacluck</a>
 							</div>
 							<h3>Community Cluck</h3>
 							<p>Lorem ipsum dolor sit amet, farhan aliqua. Ut enim ad minim veniam, quis</p>
@@ -598,12 +600,12 @@ function App() {
 
 					<div class="box">
 						<div class="img">
-							<img src="img/developer.png">
+							<img src="img/developer.png"/>
 						</div>
 						<div class="content">
 							<div class="icons">
-								<a class="fas fa-calendar"></i>3+ yrs</a>
-								<a class="fas fa-user"></i>Cluckchain  Developer</a>
+								<a class="fas fa-calendar">3+ yrs</a>
+								<a class="fas fa-user">Cluckchain  Developer</a>
 							</div>
 							<h3>Cluck'n Dev</h3>
 							<p>
@@ -682,8 +684,7 @@ function App() {
 
 			<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
 				{/* <!-- custom ja file link --> */}
-				<script src="main.js"></script>
+		</div>		<script src="main.js"></script>
 	)
 }
-
-export default App
+export default App()
